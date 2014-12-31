1449db20024586bc803730a4f8bf1e007a19d4d3
Rutger Swarts <rswarts@hostnet.nl>
2014-12-31 11:13:18 +0100

    Make the defined type able to find the setup class

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class udevnet::setup {
+class udevnet {
   concat { '/etc/udev/rules.d/70puppet-net.rules': }
   concat::fragment { "udev_network_setup_header":
     target => '/etc/udev/rules.d/70puppet-net.rules',
